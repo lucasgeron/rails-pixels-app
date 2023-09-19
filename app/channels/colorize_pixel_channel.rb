ee02b870fddb6fdf9630bed83e0826a0d9fd9b28
Lucas Fernando <lucasf.geron@icloud.com>
2023-09-19 11:55:28 -0300

    code refactor to TurboStream insted ActionCable

--- app/channels/colorize_pixel_channel.rb
+++
@@ -1,9 +0,0 @@
-class ColorizePixelChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "ColorizePixelChannel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
